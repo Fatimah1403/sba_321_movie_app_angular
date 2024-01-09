6626525689142b826d46e9f54273b83fd186ae69
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2024-01-09 12:09:53 -0500

    some features added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// import { NgbdRatingDecimal } from './rating-decimal';
+
 
 @NgModule({
   declarations: [
@@ -15,12 +19,17 @@ import { FormsModule } from '@angular/forms';
     HeaderComponent,
     HomeComponent,
     LoginComponent,
-    MovieComponent
+    MovieComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule, 
+    HttpClientModule,
+    CommonModule,
+    NgbModule,
+    // NgbdRatingDecimal
+
   ],
   providers: [],
   bootstrap: [AppComponent]
